67bd02f3127e3fc08c5e4f92dd7dfa1a7db2f22d
Silvan Wenk <silvan.wenk@stud.hslu.ch>
2020-10-12 09:34:28 +0200

    #4 logger: Use BeforeAll to configure LoggerSetup in unit tests

--- logger-component/src/test/java/ch/hslu/vsk/logger/component/VskLoggerTest.java
+++ logger-component/src/test/java/ch/hslu/vsk/logger/component/VskLoggerTest.java
@@ -7,7 +7,10 @@ package ch.hslu.vsk.logger.component;
 import ch.hslu.vsk.logger.api.LogLevel;
 import ch.hslu.vsk.logger.api.Logger;
 import ch.hslu.vsk.logger.api.LoggerSetup;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.net.InetAddress;
 
 /**
  *
@@ -15,71 +18,61 @@ import org.junit.jupiter.api.Test;
  */
 
 //TODO (hellerto): Verify Results
-public class VskLoggerTest {
+class VskLoggerTest {
+    private static LoggerSetup setup;
 
-    public VskLoggerTest() {
+    @BeforeAll
+    static void setupLogger() {
+        setup = new VskLoggerSetup();
+        setup.setLoggerName("Test");
+        setup.setMinimumLevel(LogLevel.DEBUG);
+        setup.setLoggerServer(InetAddress.getLoopbackAddress());
     }
 
     @Test
-    public void testSetMinimumLevel() {
-        LoggerSetup setup = new VskLoggerSetup();
-        setup.setMinimumLevel(LogLevel.DEBUG);
+    void testSetMinimumLevel() {
         Logger logger = setup.createLogger();
         logger.setMinimumLevel(LogLevel.WARN);
     }
 
     @Test
-    public void testTrace() {
-        LoggerSetup setup = new VskLoggerSetup();
-        setup.setMinimumLevel(LogLevel.TRACE);
+    void testTrace() {
         Logger logger = setup.createLogger();
         logger.trace("a Message");
     }
 
     @Test
-    public void testDebug() {
-        LoggerSetup setup = new VskLoggerSetup();
-        setup.setMinimumLevel(LogLevel.DEBUG);
+    void testDebug() {
         Logger logger = setup.createLogger();
         logger.debug("a Message");
     }
 
     @Test
-    public void testInfo() {
-        LoggerSetup setup = new VskLoggerSetup();
-        setup.setMinimumLevel(LogLevel.DEBUG);
+    void testInfo() {
         Logger logger = setup.createLogger();
         logger.info("a Message");
     }
 
     @Test
-    public void testWarn() {
-        LoggerSetup setup = new VskLoggerSetup();
-        setup.setMinimumLevel(LogLevel.DEBUG);
+    void testWarn() {
         Logger logger = setup.createLogger();
         logger.warn("a Message");
     }
 
     @Test
-    public void testFatal() {
-        LoggerSetup setup = new VskLoggerSetup();
-        setup.setMinimumLevel(LogLevel.DEBUG);
+    void testFatal() {
         Logger logger = setup.createLogger();
         logger.fatal("A Fatal Message", new Exception("Oh no an exception"));
     }
 
     @Test
-    public void testError() {
-        LoggerSetup setup = new VskLoggerSetup();
-        setup.setMinimumLevel(LogLevel.DEBUG);
+    void testError() {
         Logger logger = setup.createLogger();
         logger.error("A Error Message", new Exception("Oh no an exception"));
     }
 
     @Test
-    public void testLog() {
-        LoggerSetup setup = new VskLoggerSetup();
-        setup.setMinimumLevel(LogLevel.DEBUG);
+    void testLog() {
         Logger logger = setup.createLogger();
         logger.log(LogLevel.DEBUG, "A Debug Message");
     }
